6d6f7286993255e541d84e0403b978b74ef7425d
Luke McKinley <luke.mckinley21@gmail.com>
2020-04-02 10:31:12 -0500

    added walk time and a few other things to models

--- doggo_proj/activity_app/models.py
+++ doggo_proj/activity_app/models.py
@@ -11,12 +11,13 @@ class Tracker(models.Model):
     date=models.DateTimeField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    morning_walk=BooleanField()
-    afternoon_walk= BooleanField()
-    evening_walk=BooleanField()
-    breakfast=BooleanField()
-    lunch=BooleanField()
-    dinner=BooleanField()
+    morning_walk=models.BooleanField()
+    afternoon_walk= models.BooleanField()
+    evening_walk=models.BooleanField()
+    walk_time=models.DurationField()
+    breakfast=models.BooleanField()
+    lunch=models.BooleanField()
+    dinner=models.BooleanField()
 
 
 
